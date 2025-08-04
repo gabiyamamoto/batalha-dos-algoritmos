409fae6a9f33b055d96ca9f83f8a735e2a9713e5
gabi emi <gabiemi2021@gmail.com>
2025-08-04 08:47:27 -0300

    first commit

---
+++ nivel2.js
@@ -0,0 +1,13 @@
+//Atributos do personagem:
+let nomePersonagem = "Asmoday";
+let vidaAtual = 200;
+let manaAtual = 1300;
+let nivel = 16;
+let ouro = 132;
+let experiencia = 1200;
+
+//Atributos do inimigo
+let nomeInimigo = "Trojan sombrio";
+let vidaInimigo = 90;
+let poderInimigo = 50;
+
