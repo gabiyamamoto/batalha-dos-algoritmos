d8e8242f354ae1dfab8c9e706ef4ebf7b392d9b8
gabiyamamoto <gabiemi2021@gmail.com>
2025-08-05 18:06:50 -0300

    CAPÍTULO 1

--- RPG_revisao.js
+++ RPG_revisao.js
@@ -39,16 +39,19 @@ console.log(`Datheus, o quarto sistema mais próximo do Solaris, foi criado pelo
 console.log(`Todas as leis e dados que regiam a realidade eram armazenados pela Árvore Matrix.`);
 console.log(`Até que em um dia, o sistema foi assolado pelo evento catastrófico conhecido como O Reset.`);
 console.log(`Essa falha quebrou a lógica da existência dos seres que ali habitavam e enlouqueceram os criadores.`);
-console.log(`As divindades, agora corrompidas, lideram a organização 'Códice do Abismo', que busca destruir o que restou de Datheus.`);
+console.log(`As divindades, agora corrompidas, lideram a organização 'Códice do Abismo',`);
+console.log(`que busca destruir o que restou de Datheus.`);
 console.log(`Mas, seis heróis foram profetizados pelas antigas lendas a salvarem o sistema da fragmentação.`);
 console.log(`Vinda das ${origem}, ${nome} é uma ${classe} do Reino de Códrix, e uma das salvações que nasceu entre bugs.`);
-console.log(`Com suas habilidades de ${funcao} e seu ${TIPO_ARMA}, '${NOME_ARMA}', ela conduz ataques criptografados que enfraquessem trojans sombrios.`);
+console.log(`Com suas habilidades de ${funcao} e seu ${TIPO_ARMA}, '${NOME_ARMA}`);
+console.log(`ela conduz ataques criptografados que enfraquessem trojans sombrios.`);
 console.log(`${nome} é capaz de acabar com scripts virais devido ao seu nível ${nível} e um ataque total de ${ataqueTotal} pontos.`);
 console.log(`A intensificação de proteção pela ${NOME_ARMADURA} fornece um total de ${defesaTotal} pontos de defesa à ela.`);
 console.log(`Atualmente, ela acumula ${xp} XP e uma riqueza de ${ouro} de ouro.`);
 console.log(`A ${classe} nunca conseguiu ser derrubada, pois detém ${vida} pontos de vida, o maior já registrado na história do reino.`);
 console.log(`Vida suficiente? ${vidaSuficiente} | Ataque forte? ${ataqueForte} | Nível avançado? ${nivelAvancado}`);
-console.log(`Pode enfrentar a Serpente Binária? ${podeEnfrentarSerpente}. A luta pela sobrevivência da humanidade contra o Códice do Abismo ainda continua...`);
+console.log(`Pode enfrentar a Serpente Binária? ${podeEnfrentarSerpente}.`);
+console.log(`A luta pela sobrevivência da humanidade contra o Códice do Abismo ainda continua...`);
 console.log("");
 
 //== NÍVEL 2 - A BATALHA DOS ALGORITMOS ==
@@ -59,6 +62,7 @@ let classePersonagem = "arquimaga";
 let vidaAtual = 160;
 let vidaMaxima = 240;
 let manaAtual = 300;
+let manaDetectável = manaAtual * 0.1;
 let manaMaxima = 300;
 let nivelPersonagem = 16;
 let experiênciaAtual = 1350;
@@ -71,7 +75,7 @@ let agilidade = 205;
 let combatesVencidos = 20;
 
 //Estado atual da história
-let localAtual = "Reino de Códrix";
+let localAtual = "Planícies Ocultas";
 let missaoAtual = "matar o Deus do espaço, 'O Cavaleiro Silencioso'";
 
 //Prólogo - Recaptulação da história do nível 1
@@ -79,9 +83,34 @@ console.log("_________________________________________________");
 console.log("")
 console.log("🔙 PRÓLOGO DA HISTÓRIA");
 console.log("");
-console.log(`Datheus é um planeta que foi destruído pelo evento catastrófico 'O Reset'.`);
-console.log(`O evento fez com que a Árvore Matrix, que armazenava os dados que regiam a realidade, fosse contaminada e junto, as divindades que ali governavam.`);
-console.log(`${nomePersonagem} é uma ${classePersonagem} do Reino de Códrix e uma das seis salvações destinadas a matarem os deuses corrompidos.`);
-console.log(`Com sua força de ${forca} pontos, no nível ${nivelPersonagem}, e ${defesa} pontos de defesa, ${nomePersonagem} é uma das maiores esperanças para essa geração de magos.`);
-console.log(`Sua missão atual é ${missaoAtual}.`);
+console.log(`Pronta para o seu primeiro grande desafio, a ${classePersonagem} ${nomePersonagem} se aventura nas ${localAtual},`);
+console.log(`localizada no norte do Reino de Códrix.`);
+console.log(`Sua missão é ${missaoAtual}, e deter a crescente ameaça do Abismo.`);
 console.log("");
+
+//Capítulo 1 - Condicionais simples na narrativa
+console.log("");
+console.log("☘️ CAPÍTULO 1: GUILDA DE AVENTUREIROS");
+console.log("");
+
+//Verificação de xp de personagem
+if (experiênciaAtual >= 900) {
+    console.log("- Recrutamento -");
+    console.log("👀 Aventureiros veteranos te observam com interesse,");
+    console.log("considerando de recrutar seus grupos...");
+}
+
+//Verificação de ouro abundante
+if (ouro >= 100) {
+    console.log("- Comerciante -");
+    console.log("💰 Um comerciante ambulante de aborda: 'Olá nobre aventureira!");
+    console.log("Tenho diversos artefatos mágicos que talvez despertem o seu interesse.'");
+}
+
+//Verificação de classe e controle de mana
+if (classePersonagem === "arquimaga" && manaAtual >= 200 && manaDetectável <= 40) {
+    console.log("- Controle de mana -");
+    console.log("💫 Sua habilidade de controle mágico está melhorando! ");
+    console.log("Nínguem parece perceber a presença da sua mana.");
+}
+
